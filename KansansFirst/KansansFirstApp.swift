703366d94f09ee28ce503f762c72e4534bd4ba2e
Mitch Rucker <mitchruckerks@gmail.com>
2024-08-11 15:38:58 -0500

    Initial Commit

---
+++ KansansFirst/KansansFirstApp.swift
@@ -0,0 +1,32 @@
+//
+//  KansansFirstApp.swift
+//  KansansFirst
+//
+//  Created by Mitchell Rucker on 8/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KansansFirstApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
